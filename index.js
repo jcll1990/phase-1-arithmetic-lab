409c43914beffe4a6be952b95d48161eff152248
Juan Carlos Lopez Larco <Jclopezlarco@gmail.com>
2023-08-21 21:21:43 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 62
+const num2 = 1
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random())*10;
+
+const num3 = 19
+const num4 = 5
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 20)
